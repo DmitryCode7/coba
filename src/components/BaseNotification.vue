56805128a47cc660782ed2ad9f21a611ce43b21e
dmitrii <dmitri.gusev.82@mail.ru>
2025-12-28 01:05:41 +0500

    init2

---
+++ src/components/BaseNotification.vue
@@ -0,0 +1,116 @@
+<template>
+
+    <div class="notification notification--warning">
+
+        <div class="notification__icon">
+            <img src="../assets/icons/warning.svg" alt="">
+        </div>
+
+        <div class="notification__info">
+            <h3>{{ title }}</h3>
+            <p>{{ text }}</p>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'BaseNotification',
+        data() {
+            return {
+                title: 'Someting wrong',
+                text: 'Lorem ipsum dolor sit amet',
+            }
+        }
+    }
+</script>
+
+<style>
+
+    .notification {
+        display: flex;
+        gap: 20px;
+        padding: 20px 20px 16px;
+        border-radius: 8px;
+        max-width: 400px;
+    }
+
+    .notification--warning {
+        background-color: #FFF8F8;
+        border: 1px solid #F85640;
+    }
+
+    .notification--attention {
+        background-color: #FFF8EF;
+        border: 1px solid #FBA63C;
+    }
+
+    .notification--success {
+        background-color: #DCF3EB;
+        border: 1px solid #34A770;
+    }
+
+    .notification--condition {
+        background-color: #E1F2FC;
+        border: 1px solid #08A0F7;
+    }
+
+    .notification__icon {
+        width: 28px;
+        height: 28px;
+        flex-shrink: 0;
+    }
+
+    .notification__info {
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+    }
+
+    .notification__info h3 {
+        font-size: 13px;
+        font-weight: 600;
+        color: #F85640;
+    }
+
+    .notification__info p {
+        font-size: 11px;
+        font-weight: 400;
+        color: #79170A;
+    }
+
+    .notification--warning h3 {
+        color: #F85640;
+    }
+
+     .notification--warning p {
+        color: #79170A;
+    }
+
+    .notification--attention h3 {
+        color: #E18109;
+    }
+
+     .notification--attention p {
+        color: #BC741A;
+    }
+
+    .notification--success h3 {
+        color: #13854E;
+    }
+
+     .notification--success p {
+        color: #10673E;
+    }
+
+    .notification--condition h3 {
+        color: #08A0F7;
+    }
+
+     .notification--condition p {
+        color: #3387B7;
+    }
+
+</style>
